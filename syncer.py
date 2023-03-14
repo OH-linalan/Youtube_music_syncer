8bb882827bb4ff9dd08eb3852fab9635c2db7a2a
OH-linalan <linalan@kw.ac.kr>
2023-03-14 13:54:55 +0000

    make createSetting, nameInsertor function

--- syncer.py
+++ syncer.py
@@ -14,7 +14,7 @@ import os
 
 #class
 #function
-def loadSetting():
+def loadSetting():#초기 실행이 아닐 때 세팅을 불러오는 함수
     f=open("./config/setting.txt",'r')
     temp1 = f.readline()
     playlistUrl = f.readline()
@@ -26,6 +26,20 @@ def loadSetting():
         names.append(data.strip())
     return playlistUrl, names
     f.close()
+def createSetting():#초기 실행일 때 세팅을 만드는 함수
+    os.mkdir("config")
+    f = open("./config/setting.txt",'w')
+    f.write('playlist Url\n')
+    playlistUrl=input("Enter url of playlist to sync : ")
+    f.write(playlistUrl+'\n')
+    f.close()
+    print("setting complete...")
+    names = list()
+    return playlistUrl, names
+def nameInsertor(url, names):#이미 다운 받은 곡들을 배제한다
+    print('Looking for songs to download...')
+    pl = pytube.Playlist(url)
+    #url 따서 이름 추출 후 기존 names 와 비교
 #main
 print("-------------------")
 print("   Python Syncer   ")
@@ -38,5 +52,6 @@ if(os.path.isfile(currentDir+"/config/setting.txt")==True):
     print("Setting Detected...")
     print("load setting")
     playlistUrl, names = loadSetting()
-    print(playlistUrl)
-    print(names)
+else:
+    print("start setting...")
+    playlistUrl, names = createSetting()
